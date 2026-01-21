9b5c3a0cbace5a01434495d466b470dc9961ca92
SAO1215 <A01707371@tec.mx>
2026-01-20 20:05:16 -0600

    Setup global CSS system with themes and animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
 export const Home = () => {
-    return <div>HOME PAGE</div>
+    return <div className="min-h-screen">
+        
+        {/* Theme Toggle */}
+
+        {/* Background effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+
+        </div>
 }
