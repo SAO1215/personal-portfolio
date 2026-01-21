06ed2d910829187cf112deb3c4818fd001aa49ea
SAO1215 <A01707371@tec.mx>
2026-01-20 18:31:07 -0600

    Add Tailwind and core dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  // set an alias for the source folder, so we can we can import components using @ simbol intead of directly reference the source folder
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
 })
