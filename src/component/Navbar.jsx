7918943403b9b75f3f35c8b8a4d16a1cb02188d4
SAO1215 <A01707371@tec.mx>
2026-01-22 20:18:04 -0600

    Add responsive navbar with scroll and mobile menu

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -50,7 +50,7 @@ export const Navbar = () => {
                     href="#hero"
                 >
                     <span className="relative z-10">
-                        <span className="text-glow text-foreground"> PedroTech </span>{" "}
+                        <span className="text-glow text-foreground"> Olivia </span>{" "}
                         Portfolio
                     </span>
                 </a>
